3e2a7355c1a06782fbbe60f1dee32536dbc0165e
meru_akimbo <merukatoruayu0@gmail.com>
2014-07-08 20:30:28 +0900

    make mock

--- lib/Teng/Plugin/Pager/Cache/Redis.pm
+++ lib/Teng/Plugin/Pager/Cache/Redis.pm
@@ -2,9 +2,78 @@ package Teng::Plugin::Pager::Cache::Redis;
 use 5.008001;
 use strict;
 use warnings;
+use Class::Accessor::Lite rw => [qw/redis_config/];
+use Redis;
+use Data::Page;
 
 our $VERSION = "0.01";
 
+our @EXPORT = qw/group_key insert_and_update_pager update_with_pager delete_with_pager fetch_with_pager/;
+
+sub group_key {
+    my ($self, $table_name, $order_by_key) = @_;
+    return sprintf('pager-%s-%s', $table_name, $order_by_key->{order_by_key});
+}
+
+sub insert_and_update_pager {
+    my ($self, $table_name, $value, $order_by, $prymary_key) = @_;
+
+    my $order_by_key = $order_by->{order_by_key};
+    my $row = $self->insert($table_name => $value);
+    $self->_redis->excute(
+        ZADD => $self->group_key($table_name, $order_by), $value->{$order_by_key}, $prymary_key->{primary_key}
+    );
+
+    return $row;
+}
+
+sub update_with_pager {
+    my ($self, $table_name, $value, $order_by, $prymary_key) = @_;
+
+    my $order_by_key = $order_by->{order_by_key};
+    my $row = $self->update($value);
+    $self->_redis->excute(
+        ZADD => $self->group_key($table_name, $order_by), $value->{$order_by_key}, $prymary_key->{primary_key}
+    );
+
+    return $row;
+}
+
+sub delete_with_pager {
+    my ($self, $order_by) = @_;
+
+    my $order_by_key = $order_by->{order_by_key};
+
+    $self->delete();
+    $self->_redis->excute(
+        ZREM => $self->group_key($self->{table_name}, $order_by), $self->prymary_keys
+    );
+}
+
+sub fetch_with_pager {
+    my ($self, $table_name, $order_by, $prymary_key, $args) = @_;
+
+    my $order_by_key = $order_by->{order_by_key};
+
+    my $limit  = $args->{row};
+    my $offset = ($args->{page} - 1 ) * $limit;
+
+    my @primary_keys = $self->_redis->excute(ZRANGE => $self->group_key($table_name, $order_by_key), $offset, $limit);
+    my @rows  = $self->search($table_name, [ $prymary_key => { IN => \@primary_keys } ], { order_by => 'created' });
+    my $count = $self->_redis->excute('ZCARD' => $self->group_key($table_name, $order_by_key));
+
+    my $pager = Data::Page->new();
+    $pager->entries_per_page($limit);
+    $pager->current_page($args->{page});
+    $pager->total_entries($count);
+
+    return (\@rows, $pager);
+}
+
+sub _redis {
+    my ($self,) = @_;
+    return Redis->new(%{ $self->redis_config });
+}
 
 
 1;
